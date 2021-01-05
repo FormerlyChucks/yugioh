8b4d7fae8660e9df9a749b60cf3284da5ec7e0fc
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-01-05 10:05:34 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+        
+setuptools.setup(name='yugioh',
+                 version='0.0.22',
+                 description='Yu-Gi-Oh API Wrapper',
+                 long_description=readme(),
+                 long_description_content_type='text/markdown',
+                 url='https://github.com/IThinkImOKAY/yugioh',
+                 author='diogenesjunior',
+                 author_email='diogenesjunior@protonmail.com',
+                 packages=['yugioh'],
+                 keywords='Yu-Gi-Oh API')
